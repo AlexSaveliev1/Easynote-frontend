00ed3154c11a45f3d741cc72654b2c566e63bf82
Sasha Saveliev <airbasic4@gmail.com>
2017-05-30 22:25:12 +0300

    tmp

--- app/components/header-bar.js
+++ app/components/header-bar.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['header-bar-wrapper']
+  classNames: ['header-bar-wrapper'],
+  router: Ember.inject.service('-routing'),
+
+  actions: {
+    transitionToSignIn() {
+      this.get('router').transitionTo('sign-in');
+    }
+  }
 });
